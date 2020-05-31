8260c79c9d4893661ec3c97f52a56467cbd451f9
alisadekk <alysadekk@gmail.com>
2020-05-31 20:07:15 +0200

    Updates

---
+++ precache-manifest.76738c18f5bd01892710bc5f053e0337.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a6326e788396accdb1a251716d10bc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "29bab644badd52d94afa",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "a03bcf2657787b40906d",
+    "url": "/portfolio/static/css/main.be7a0c7f.chunk.css"
+  },
+  {
+    "revision": "29bab644badd52d94afa",
+    "url": "/portfolio/static/js/2.6ebbed75.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.6ebbed75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03bcf2657787b40906d",
+    "url": "/portfolio/static/js/main.f2aa4aff.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "d2dfd99e5a5917bdfc8d2e8803025208",
+    "url": "/portfolio/static/media/Ali Face.d2dfd99e.png"
+  },
+  {
+    "revision": "baac890a613fed83b935c89a90a0530b",
+    "url": "/portfolio/static/media/Software Engineer CV Design-01.baac890a.svg"
+  },
+  {
+    "revision": "b1fe8980fb45104b8a6c61a71273fd4e",
+    "url": "/portfolio/static/media/Software Engineer CV Design-02.b1fe8980.svg"
+  }
+]);
